0b624a30352a73480ed7187bc8102ff6ca9c4d2c
Inseong Park <rariradev@gmail.com>
2024-06-12 21:40:00 +0900

    SW-5 Try: fix

--- apps/web/app/(external)/page.tsx
+++ apps/web/app/(external)/page.tsx
@@ -6,7 +6,7 @@ import ContactSection from './_components/Sections/Contact';
 import IntroSection from './_components/Sections/Intro';
 import ProcessSection from './_components/Sections/Process';
 
-export default function Page(): JSX.Element {
+export default async function Page(): Promise<JSX.Element> {
   return (
     <main className="flex-col w-full">
       <Hero />
