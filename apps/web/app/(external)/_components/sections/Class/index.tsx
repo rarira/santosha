5063710b466b89a36c5551e719adc32eb4027966
Inseong Park <rariradev@gmail.com>
2024-06-12 16:54:47 +0900

    SW-9 Add: Section

---
+++ apps/web/app/(external)/_components/sections/Class/index.tsx
@@ -0,0 +1,12 @@
+import SectionContainer from '../_components/SectionContainer';
+import SectionTitle from '../_components/SectionTitle';
+
+function ClassSection(): JSX.Element {
+  return (
+    <SectionContainer sectionName="class">
+      <SectionTitle title="요가 수업 종류" subtitle="다양한 종류의 요가를 함께 할 수 있어요" />
+    </SectionContainer>
+  );
+}
+
+export default ClassSection;
